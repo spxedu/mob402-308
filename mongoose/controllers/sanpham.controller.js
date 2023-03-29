03cfa6c0ad8d400ec6e125fa760c84ce15958691
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-29 13:50:13 +0700

    Lọc theo giá, sửa upload file

--- mongoose/controllers/sanpham.controller.js
+++ mongoose/controllers/sanpham.controller.js
@@ -4,7 +4,17 @@ const myModel = require('../models/sanpham.model');  // nhúng thư viện Model
 exports.list = async (req,res, next)=>{
     // hiển thị danh sách sản phẩm
 
-    var list = await myModel.spModel.find().sort( { name: 1 });// tìm sp
+    // chức năng lọc: 
+    // kiểm tra tồn tại tham số
+    let dieu_kien = null;
+
+    if(typeof( req.query.price) != 'undefined' )
+    {
+        let price = req.query.price; 
+        dieu_kien = { price: price };
+    }
+
+    var list = await myModel.spModel.find(  dieu_kien   ).sort( { name: 1 });// tìm sp
 
     res.render('sanpham/list', { listSp: list }); // truyền DS ra view
 }
@@ -17,20 +27,19 @@ exports.add = async (req, res, next)=>{
         // xử lý upload 
         console.log(req.file, req.body);
         // sử dụng hàm fs.rename để di chuyển file
-        await fs.promises.rename(req.file.path, 
-                  './public/uploads/'+req.file.originalname,
-                       (err)=>{
-                        console.log(err);
-                        if(err != null){
-                            console.log(err);
-                        }else{
-                            url_image = '11111';
-                            //không có lỗi ==> upload thành công
-                             url_image =  '/uploads/'+ req.file.originalname ;
-                             console.log("upload thanh cong" + url_image);
-                        }
-                    })
+        try {
+            await fs.rename(req.file.path,  './public/uploads/'+req.file.originalname )
+                  // đến dưới này là thành công
+                  
+                  //không có lỗi ==> upload thành công
+                   url_image =  '/uploads/'+ req.file.originalname ;
+                   console.log("upload thanh cong" + url_image);
+                     
+        } catch (error) {
+            // nếu có lỗi thì xảy ra lỗi ở đây
 
+        }
+         
     }
 
     
--- mongoose/models/db.js
+++ mongoose/models/db.js
@@ -7,3 +7,12 @@ mongoose.connect('mongodb://localhost:27017/ql_banhang')
         });
 
 module.exports = { mongoose }; 
+
+// Mở notepad (run với quyền Admin)
+//==> file ==> Open ==> thư mục: C:\Windows\System32\Driver\etc\
+// trên cửa sổ phần chọn file Type: chọn all 
+// chọn file :  hosts    ==> nhìn thấy dòng 127.0.0.1 bị dấu # ở đầu , xóa dấu #
+// sau đó lưu lại
+// Trường hợp sửa file hosts không được: thay đổi code trực tiếp
+// mongoose.connect('mongodb://localhost:27017/ql_banhang')
+//               ==> thay chữ: localhost = 127.0.0.1
